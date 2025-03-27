7590b257828e9f9856efcf875ab331528347ffb5
Eric Sison <ericsison.dev@gmail.com>
2025-03-28 00:37:53 +0800

    added CRUD functionality for the /users route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,25 @@
 import express from "express";
+import { Request, Response } from "express";
 import { schema } from "../mongodb";
+
+export type UserParams = {
+  id: string;
+};
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
+router.get("/", async (req, res) => {
+  try {
+    const users = await schema.User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+    }
+  }
+});
+
+router.post("/", async (req: Request, res: Response) => {
   try {
     const user = await schema.User.create(req.body);
     res.status(201).json(user);
@@ -14,13 +30,24 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.get("/", async (req, res) => {
+router.put("/", async (req: Request, res: Response) => {
   try {
-    const users = await schema.User.find();
-    res.status(200).json(users);
+    const user = await schema.User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json(user);
   } catch (error) {
     if (error instanceof Error) {
-      res.status(500).json({ message: error.message });
+      res.status(400).json({ message: error.message });
+    }
+  }
+});
+
+router.delete("/", async (req: Request, res: Response) => {
+  try {
+    await schema.User.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "User deleted!" });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(400).json({ message: error.message });
     }
   }
 });
